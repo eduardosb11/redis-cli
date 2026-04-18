e3b0afb457662fb145b27bd4eff14059ed5a61ae
eduardosb11 <barbosa.eduardo11@outlook.com>
2026-04-18 20:23:31 -0300

    feat: start tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Could not bind to port");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("error: {}", { e });
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let pong = b"+PONG\r\n";
+    stream.write_all(pong).expect("Failed to write to stream");
 }
